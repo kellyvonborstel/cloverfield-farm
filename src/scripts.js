2466e7d9fe9b808802f23bf4ed061bab38fac61f
Kelly von Borstel <kellyvonborstel@gmail.com>
2018-11-18 18:47:17 -0800

    Fix turn card function

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,27 @@
-var cards = document.querySelectorAll('.game-card');
-
-function turnCard() {
-  // console.log(this);
-  this.classList.toggle('turn')
-}
-
-cards.forEach(card => card.addEventListener('click', turnCard));
+document.addEventListener("DOMContentLoaded", function(event) {
+  // console.log("DOM fully loaded and parsed");
+  var cards = document.querySelectorAll('.game-card');
+  // console.log(cards);
+  
+  var hasTurnedCard = false;
+  var firstCard;
+  var secondCard;
+  
+  function turnCard() {
+    this.classList.add('turn');
+    if (!hasTurnedCard) {
+      hasTurnedCard = true;
+      firstCard = this;
+    } else {
+      hasTurnedCard = false;
+      secondCard = this;
+    }
+    // this.classList.toggle('turn')
+  }
+  
+  
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', turnCard);
+    console.log(cards[i]);
+  }
+});
