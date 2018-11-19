a39785cfe0fa7552f484cd98ce2335fc19de374a
Kelly von Borstel <kellyvonborstel@gmail.com>
2018-11-18 22:10:15 -0800

    Complete game and add instruction modal

--- src/scripts.js
+++ src/scripts.js
@@ -1,38 +1,52 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  // console.log("DOM fully loaded and parsed");
-  var cards = document.querySelectorAll('.game-card');
-  for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', turnCard);
-    console.log(cards[i]);
-  }
-  
-  var hasTurnedCard = false;
-  var firstCard;
-  var secondCard;
-  
-  function turnCard() {
-    this.classList.add('turn');
-    if (!hasTurnedCard) {
-      hasTurnedCard = true;
-      firstCard = this;
-      return;
-    } else {
-      hasTurnedCard = false;
-      secondCard = this;
-      compareCards(); 
-    }
-  }
 
-  function compareCards() {
-    if (firstCard.dataset.veg === secondCard.dataset.veg) {
-      firstCard.removeEventListener('click', turnCard);
-      secondCard.removeEventListener('click', turnCard);
-    } else {
-      setTimeout(function() {
-        firstCard.classList.remove('turn');
-        secondCard.classList.remove('turn');
-      }, 2000);
+// game is based on this tutorial https://www.youtube.com/watch?v=ZniVgo8U7ek
+
+$(function() {
+  var cardTurned = false;
+  var boardLocked = false;
+  var card1;
+  var card2;
+
+  $('.game-card').on('click', turnCard);
+  $('.game-card').each(function() {
+    var position = Math.floor(Math.random() * 12);
+    $(this).style.order = position;
+  });
+
+  function turnCard() {
+    if (!boardLocked && !(this === card1)) {
+      $(this).addClass('turn');
+      if (!cardTurned) {
+        cardTurned = true;
+        card1 = this;
+        return;
+      }
+      card2 = this;
+      compareCards();
     }
+  }
+  
+  function compareCards() {
+    var card1Veg = card1.getAttribute('data-veg');
+    var card2Veg = card2.getAttribute('data-veg');
+    if (card1Veg === card2Veg) {
+      $(card1).off();
+      $(card2).off();
+      reset();
+    } else {
+      boardLocked = true;
+      setTimeout(() => {
+        $(card1).removeClass('turn');
+        $(card2).removeClass('turn');
+        reset();
+      }, 1000);
+    }
+  }
+  
+  function reset() {
+    cardTurned = false;
+    boardLocked = false;
+    card1 = null;
+    card2 = null;
   }
 });
-
